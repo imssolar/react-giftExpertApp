4fe8d4c984c4fb68a1bb7896b1dedee393d57387
Ivan Montecinos <i.montecinossolar@gmail.com>
2020-09-13 13:46:03 -0300

    docs agregado

---
+++ docs/precache-manifest.d34f2a451c1a9f6948d4489d688ddbc6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f585e0e73c46cb106147",
+    "url": "/static/css/main.e654398e.chunk.css"
+  },
+  {
+    "revision": "f585e0e73c46cb106147",
+    "url": "/static/js/main.15af6915.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f2038e419ec47478ca17",
+    "url": "/static/js/2.984c9d44.chunk.js"
+  },
+  {
+    "revision": "508062593032ae312788ea0d3082acc3",
+    "url": "/index.html"
+  }
+];
